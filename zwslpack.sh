227bf29a55b1e68defe73c7170f48ab8abb87d9c
Zach Fortier <fort0142@umn.edu>
2019-02-20 02:38:04 -0600

    Create zwslpack.sh
    Beginning of a bash final to initialize WSL on windows, hope to be able to do a clean install with ease. Also, could look into the option of having both desktop and laptop wsl have the same dependencies.

---
+++ zwslpack.sh
@@ -0,0 +1,17 @@
+sudo apt update &&
+sudo apt upgrade
+sudo apt-get -y install make &&
+sudo apt-get -y install unzip &&
+sudo apt-get -y install gcc &&
+sudo apt-get -y install valgrind &&
+sudo apt-get -y install default-jdk &&
+sudo apt-get -y install default-jre &&
+sudo apt-get -y install vim &&
+sudo apt-get -y install ssh xauth xorg &&
+sudo add-apt-repository -y ppa:git-core/ppa &&
+sudo apt-get -y install git &&
+sudo apt-get -y install openssh-server openssh-client
+sudo apt upgrade
+sudo apt-get autoremove
+# sudo sh -c 'cat sshsettings.txt >> /etc/ssh/ssh_config'
+
