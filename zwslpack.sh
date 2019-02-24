8f020d0af8982b4a45fee430b9e7084d9695f6ba
monsieur40a <33198723+monsieur40a@users.noreply.github.com>
2019-02-24 16:03:07 -0600

    force yes

--- zwslpack.sh
+++ zwslpack.sh
@@ -1,5 +1,5 @@
-sudo apt update &&
-sudo apt upgrade
+sudo apt -y update &&
+sudo apt -y upgrade
 sudo apt-get -y install make &&
 sudo apt-get -y install unzip &&
 sudo apt-get -y install gcc &&
@@ -11,7 +11,8 @@ sudo apt-get -y install ssh xauth xorg &&
 sudo add-apt-repository -y ppa:git-core/ppa &&
 sudo apt-get -y install git &&
 sudo apt-get -y install openssh-server openssh-client
-sudo apt upgrade
-sudo apt-get autoremove
+sudo apt -y update
+sudo apt -y upgrade
+sudo apt -y autoremove
 # sudo sh -c 'cat sshsettings.txt >> /etc/ssh/ssh_config'
 
